5b4271eb3cc1e15c7ffc4fde0cdfd1f44a479d10
Sudarshan Gaikwad <sudarshan.gaikwad@siemens.com>
2018-04-09 20:41:08 +0530

    added four squares, changed css for primary button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from './components/router.module';
 import { FooterComponent } from './components/footer/footer.component';
+import { MainBodyComponent } from './components/main-body/main-body.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    MainBodyComponent,
   ],
   imports: [
     BrowserModule,
